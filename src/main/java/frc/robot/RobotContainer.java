7db2b8949a61bc83266ee7aa71a03930eda87b08
Shaun <Shaunfruggie1257@gmail.com>
2024-01-16 02:38:37 -0500

    Added Orchestra

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,6 +6,9 @@ package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.subsystems.DualFlyWheelSubsystem;
+import frc.robot.subsystems.OrchestraSubsystem;
+import frc.robot.subsystems.OrchestraSubsystem.Song;
+
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj2.command.Command;
@@ -16,11 +19,12 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 public class RobotContainer {
 
   //#region Devices 
-  TalonFX flywheelLeft, flywheelRight;   
+  TalonFX flywheelLeft, flywheelRight, bassGuitar;   
   //#endregion
 
   //#region Subsystems
   DualFlyWheelSubsystem flywheel;
+  OrchestraSubsystem daTunes;
   //#endregion Subsystems
 
   private final CommandXboxController m_driverController =
@@ -30,10 +34,16 @@ public class RobotContainer {
 
     flywheelLeft = new TalonFX(Constants.SystemConstants.leftFlywheelCAN);
     flywheelRight = new TalonFX(Constants.SystemConstants.rightFlywheelCAN);
+    //Orchestra
+    bassGuitar = new TalonFX(Constants.SystemConstants.bassGuitar);
+
     flywheel = new DualFlyWheelSubsystem(flywheelLeft, flywheelRight);
     flywheel.SetStatePower(0.2);
     flywheel.SetRatio(0.8);
 
+    daTunes = new OrchestraSubsystem(new TalonFX[]{bassGuitar});
+    daTunes.SetTune(Song.ONE_ONE_FIVE );
+    
 
     // Configure the trigger bindings
     configureBindings();
@@ -42,7 +52,10 @@ public class RobotContainer {
   // tl;dr: Trigger class for simple booleans
   private void configureBindings() {
     
-    m_driverController.b().onTrue(new InstantCommand( () -> flywheel.Toggle(), flywheel));
+    //WOW This is bad but oh well
+    m_driverController.y().onTrue(new InstantCommand( () -> flywheel.Toggle(), flywheel));
+    m_driverController.b().onTrue(new InstantCommand( () -> daTunes.Play(), daTunes));
+    m_driverController.a().onTrue(new InstantCommand( () -> daTunes.Shud(), daTunes));
     
   }
   public Command getAutonomousCommand() {
