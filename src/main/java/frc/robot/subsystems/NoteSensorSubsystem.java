1c0cc9f3d17ec32487c5f704350297941e5e7874
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2024-01-25 00:03:18 -0500

    starting to setup the NoteSub and commands

--- src/main/java/frc/robot/subsystems/NoteSensorSubsystem.java
+++ src/main/java/frc/robot/subsystems/NoteSensorSubsystem.java
@@ -1,13 +1,20 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 /* Subsystem class to primarily use a Time of Flight sensor from 'Playing with Fusion'.
  * It will read the distance from the sensor to the 'note' and determine if the note is in a load position.
  * This sensor data will change LED status and enable/disable intake and index motors
  */
-public class NoteSensorSubsystem {
+public class NoteSensorSubsystem extends SubsystemBase{
     
     /* Constructor */
     public NoteSensorSubsystem() {
+    }
+
+    public void isNoteLoaded() {
+        // Code to run repeatedly while the command is scheduled to run
+        
     }
 
 } // end of class NoteSensorSubsystem
