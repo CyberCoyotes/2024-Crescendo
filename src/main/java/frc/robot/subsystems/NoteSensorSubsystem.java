8068efb00fa4b9897e317f2efb556737b0c95760
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2024-01-25 00:07:18 -0500

    documentation updates

--- src/main/java/frc/robot/subsystems/NoteSensorSubsystem.java
+++ src/main/java/frc/robot/subsystems/NoteSensorSubsystem.java
@@ -2,8 +2,10 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-/* Subsystem class to primarily use a Time of Flight sensor from 'Playing with Fusion'.
+/* 
+ * Subsystem class to primarily use a Time of Flight sensor from 'Playing with Fusion'.
  * It will read the distance from the sensor to the 'note' and determine if the note is in a load position.
+ * It should return an actual distance reading to the SmartDashboard and a boolean for a method 'isNoteLoaded'
  * This sensor data will change LED status and enable/disable intake and index motors
  */
 public class NoteSensorSubsystem extends SubsystemBase{
