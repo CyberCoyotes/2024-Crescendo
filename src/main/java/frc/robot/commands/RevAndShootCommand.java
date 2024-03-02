303fca6b0da405718cce5d94e6abbc97507cee3c
Shaun <Shaunfruggie1257@gmail.com>
2024-03-02 11:55:49 -0500

    Add RevAndShootCommand v1

--- src/main/java/frc/robot/commands/RevAndShootCommand.java
+++ src/main/java/frc/robot/commands/RevAndShootCommand.java
@@ -2,10 +2,10 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
+
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.subsystems.IndexSubsystem;
 import frc.robot.subsystems.ShooterSubsystemVelocity;
@@ -15,23 +15,22 @@ import frc.robot.subsystems.ShooterSubsystemVelocity;
  */
 public class RevAndShootCommand extends SequentialCommandGroup {
     // The time to try to and shoot the cargo before executing..
-    public int TimeToWait = 1;
     // subsystems
     IndexSubsystem indexer;
     ShooterSubsystemVelocity shooter;
     // component Commands
 
-    private Command revUpShooter;
     private Command indexCommand;
 
-    public RevAndShootCommand(IndexSubsystem indexer) {
-
+    public RevAndShootCommand(IndexSubsystem indexer, ShooterSubsystemVelocity shooter) {
+        addRequirements(indexer, shooter);
+        indexCommand = new RunCommand(() -> indexer.RunIndexing(), indexer);
+        this.shooter = shooter;
         SetupCommands();
 
     }
 
     private void SetupCommands() {
-
         this.addCommands(new ParallelCommandGroup(new RunCommand(() -> shooter.SetOutput(100), shooter),
                 new SequentialCommandGroup(new WaitUntilCommand(() -> shooter.AtVelocity(100))).andThen(indexCommand)));
 
