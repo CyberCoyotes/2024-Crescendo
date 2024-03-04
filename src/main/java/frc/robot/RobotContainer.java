16b72116d08b9854f4bbbd4b4d7714541a7e3486
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2024-03-03 23:04:59 -0500

    auton clean up

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -119,7 +119,7 @@ public class RobotContainer {
     SmartDashboard.putData("Auto Chooser", autoChooser);
     /* Unintended side effect is this will create EVERY auton file from the RIO deploy folder. See solution below */
 
-    indexIncrent = new IncrementIndex1Stage(index);
+    // FIXME indexIncrent = new IncrementIndex1Stage(index);
 
     shooter = new ShooterSubsystemVelocity();
 
@@ -204,7 +204,7 @@ public class RobotContainer {
     // var driverDiagnostics = Shuffleboard.getTab("Driver Diagnostics");
     var driverDiagnostics = Shuffleboard.getTab("Driver Diagnostics");
 
-    driverDiagnostics.add("Test Auton", autonTesting);
+    // driverDiagnostics.add("Test Auton", autonTesting);
     driverDiagnostics.add(autoChooser);
 
     driverDiagnostics.addBoolean("Note Detected", () -> index.HasCargo());
