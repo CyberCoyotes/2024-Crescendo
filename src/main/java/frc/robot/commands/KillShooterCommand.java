df3f7a2b68e1b98b0eb7dccc52d7a192fe7e2528
Shaun <Shaunfruggie1257@gmail.com>
2024-03-05 17:40:18 -0500

    Add untested KillShooterCommand

---
+++ src/main/java/frc/robot/commands/KillShooterCommand.java
@@ -0,0 +1,28 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ShooterSubsystemVelocity;
+
+public class KillShooterCommand extends Command {
+    private ShooterSubsystemVelocity shooter;
+
+    public KillShooterCommand(ShooterSubsystemVelocity shooterArg) {
+        shooter = shooterArg;
+        addRequirements(shooter);
+    }
+
+    @Override
+    public void initialize() {
+        shooter.Disable();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        shooter.Disable();
+    }
+}
