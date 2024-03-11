015311c32f2a52f260ee915917f57f9d937d4191
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2024-03-11 19:35:53 -0400

    Remove unused import statement

--- src/main/java/frc/robot/subsystems/ShooterSubsystemVelocity.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystemVelocity.java
@@ -10,7 +10,6 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.networktables.GenericEntry;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
