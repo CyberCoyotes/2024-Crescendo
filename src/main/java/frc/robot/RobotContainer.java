69dfec353e44fb31404ece04d95eecdbeace4f34
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2024-03-11 14:14:43 -0400

    Remove unused code and update comments

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,7 +4,6 @@
 
 package frc.robot;
 
-
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
 import com.pathplanner.lib.auto.AutoBuilder;
@@ -36,7 +35,7 @@ import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystemVelocity;
 import frc.robot.subsystems.WinchSubsystem2;
 
-// Getting rid of the soft yelling
+// Getting rid of the soft unused warnings
 @SuppressWarnings("unused")
 public class RobotContainer {
 
@@ -47,9 +46,6 @@ public class RobotContainer {
   // #endregion
   // #region Network Tables
 
-  // Interactable way to change increment distance on arm for High Speed High
-  // Fidelity Testing
-
   // #endregion Network Tables
   // #region Subsystems
 
@@ -57,8 +53,7 @@ public class RobotContainer {
   ShooterSubsystemVelocity shooter = new ShooterSubsystemVelocity();
   IntakeSubsystem intake = new IntakeSubsystem();
   IndexSubsystem index = new IndexSubsystem();
-  // OrchestraSubsystem daTunes;
-  WinchSubsystem2 winch = new WinchSubsystem2(); // TODO Test
+  WinchSubsystem2 winch = new WinchSubsystem2();
   ArmSubsystem arm = new ArmSubsystem();
   // #endregion Subsystems
 
@@ -90,7 +85,6 @@ public class RobotContainer {
 
   private final IntakeCommandGroup intakeGroup = new IntakeCommandGroup(index, intake);
   private final IntakeRevCommandGroup intakeRevGroup = new IntakeRevCommandGroup(index, intake);
-  //final SetIndex indexing = new SetIndex(index, power);
 
   /* Autonomous Chooser*/
   SendableChooser<Command> autoChooser;
@@ -99,59 +93,33 @@ public class RobotContainer {
   public RobotContainer() {
 
     /*Pathplanner Named Commands*/
-            
-    // NamedCommands.registerCommand("RunShooter", new RunShooter(shooter));
-    // NamedCommands.registerCommand("Shoot", new ShooterIndex(shooter, index));
     NamedCommands.registerCommand("ShootClose", new ShootClose(arm, index, intake, shooter));
-    // NamedCommands.registerCommand("StopShooting", new StopShooting(arm, index, intake, shooter));;
     NamedCommands.registerCommand("Intake", new IntakeIndex(index, intake));
-    // NamedCommands.registerCommand("StopIntake", new StopIntakeIndex(index, intake));
-    // NamedCommands.registerCommand("ArmHome", new SetArmPosition(arm, Constants.ArmConstants.ARM_HOME_POSE));
-    // NamedCommands.registerCommand("ArmLow", new SetArmPosition(arm, Constants.ArmConstants.ARM_LOW_POSE));
     // NamedCommands.registerCommand("ArmMid", new SetArmPosition(arm, Constants.ArmConstants.ARM_MID_POSE));
     
-    /* Auto Chooser */
+    /* Auto Chooser
+    * Unintended side effect is this will create EVERY auton file from the RIO deploy folder.
+    * FTP into the the RIO to delete old auton options */
     autoChooser = AutoBuilder.buildAutoChooser();
     SmartDashboard.putData("Auto Chooser", autoChooser);
-    /* Unintended side effect is this will create EVERY auton file from the RIO deploy folder. See solution below */
 
-    // FIXME I don't know if this is needed but seems undefined 
-    // indexIncrent = new IncrementIndex1Stage(index);
-    
     shooter = new ShooterSubsystemVelocity();
-
     index.setDefaultCommand(index.run(() -> index.SetPower(BumperStatus(1))));
 
-/* 
-
-    arm.setDefaultCommand(
-        arm.run(() -> arm.Drive(((m_operatorController.axisLessThan(Axis.kLeftY.value,
-            -0.1).getAsBoolean() ||
-            (m_operatorController.axisGreaterThan(Axis.kLeftY.value, 0.1))
-                .getAsBoolean()) ? m_operatorController.getLeftY() : 0))));
-*/
-
-    // // intake run depending on driver bumper status
-    // ORIGINAL intake.setDefaultCommand(intake.run(() -> intake.Run(0.75 *
-    // -BumperStatus(0))));
-    // intake.setDefaultCommand(intakeGroupCommand);
-
-    index.setDefaultCommand(index.run(() -> index.SetPower(BumperStatus(1))));
-    /*
-     * shooter.setDefaultCommand(shooter.run(() -> shooter.SetOutput(
-     * // ! cool but unintuitive
-     * Math.max(m_operatorController.getLeftTriggerAxis() * 0.5 * 60,
-     * m_operatorController.getRightTriggerAxis() * 60))));
-     */
+    /* Configure the Button Bindings */
     configureBindings();
+
     DebugMethodSingle();
+
   }
 
-
+  /* Method to check our Alliance status for Red or Blue
+   * This is essential for the autonomous mode to know which side of the field we are on */
   public static boolean isAllianceRed() {
     return DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Red;
   }
 
+  /* Method to configure the button bindings */
   private void configureBindings() {
 
     drivetrain.setDefaultCommand( // Drivetrain will execute this command periodically
@@ -175,7 +143,7 @@ public class RobotContainer {
     -m_driverController.getLeftX()))));
     */
 
-        m_driverController.start().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldRelative()));
+    m_driverController.start().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldRelative()));
     m_driverController.a().whileTrue(new InstantCommand(() -> arm.setArmPose(Constants.ArmConstants.ARM_HOME_POSE)));
     m_driverController.b().whileTrue(new InstantCommand(() -> arm.setArmPose(Constants.ArmConstants.ARM_MID_POSE)));
     m_driverController.x().whileTrue(new InstantCommand(() -> arm.setArmPose(Constants.ArmConstants.ARM_AMP_POSE)));
@@ -187,12 +155,15 @@ public class RobotContainer {
     m_driverController.rightTrigger().whileFalse(new InstantCommand(() -> shooter.SetOutput(0)));
     m_driverController.leftTrigger().whileTrue(new SetIndex(index,-0.75));
 
+    // m_operatorController.a().whileTrue (new));
+    m_operatorController.b().whileTrue(new SetArmClimb(arm,Constants.ArmConstants.ARM_MANUAL_POWER));
+    // m_operatorController.x().whileTrue (new));
     m_operatorController.y().whileTrue(new SetWinch(winch, Constants.WinchConstants.WINCH_POWER));
     m_operatorController.back().whileTrue(new SetWinch(winch, Constants.WinchConstants.WINCH_POWER_BOOST));
-    m_operatorController.b().whileTrue(new SetArmClimb(arm,Constants.ArmConstants.ARM_MANUAL_POWER));
-   
+    
   };
 
+  /* Use for Debugging purposes */
   public void DebugMethodSingle() {
     // #region Driving
 
@@ -229,4 +200,5 @@ public class RobotContainer {
   public Command getAutonomousCommand() {
     return autoChooser.getSelected();
   }
+
 } // End of class
