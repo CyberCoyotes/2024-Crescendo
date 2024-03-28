bafe442abcd6490302383d68e57bb0fcb5a98623
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2024-03-28 14:21:16 -0400

    adusting ShootClose2 for auton

--- src/main/java/frc/robot/experimental/ShootClose2.java
+++ src/main/java/frc/robot/experimental/ShootClose2.java
@@ -1,27 +1,20 @@
 package frc.robot.experimental;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.commands.SetIndex;
-import frc.robot.commands.SetFlywheel;
+import frc.robot.commands.SetArmPosition;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.IndexSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.NoteSensorSubsystem;
 import frc.robot.subsystems.ShooterSubsystem2;
-import frc.util.Constants;
+import frc.util.Constants.ArmConstants;
 
 
-public class ShootClose2 extends ParallelCommandGroup{
+public class ShootClose2 extends SequentialCommandGroup{
     public ShootClose2(ArmSubsystem arm, IndexSubsystem index, IntakeSubsystem intake, ShooterSubsystem2 shooter2, NoteSensorSubsystem notesensor) {
 
         addCommands(
-            /* Moving the arm is shouldn't be necessary, but just in case */
-            // new SetArmPosition(arm, Constants.ArmConstants.ARM_HOME_POSE),
-            // new ShooterIndex(shooter, index).withTimeout(1.0)
-
-            // new SetFlywheel(shooter2, arm, 60),
+            new SetArmPosition(arm, ArmConstants.ARM_HOME_POSE),
             new ShootWhenReady(shooter2, index, notesensor)
 
         );
