52327bfb737adc1d213a3d7de629bf269cb17b17
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2024-04-03 12:44:40 -0400

    auto amp updates

--- src/main/java/frc/robot/experimental/AutoShootAmp.java
+++ src/main/java/frc/robot/experimental/AutoShootAmp.java
@@ -1,58 +1,23 @@
 package frc.robot.experimental;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.ShootAmp;
 import frc.robot.subsystems.IndexSubsystem;
 import frc.robot.subsystems.ShooterSubsystem2;
 import frc.robot.util.Constants;
 import frc.robot.util.IndexConstants;
 import frc.robot.subsystems.NoteSensorSubsystem;
 import frc.robot.util.ShooterConstants;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import java.util.concurrent.TimeUnit;
 
 @SuppressWarnings("unused")
 
-public class AutoShootAmp extends Command {
-    private ShooterSubsystem2 shooter2; 
-    private IndexSubsystem index;
-    private NoteSensorSubsystem notesensor;
-
+public class AutoShootAmp extends SequentialCommandGroup {
     public AutoShootAmp(ShooterSubsystem2 shooter2, IndexSubsystem index, NoteSensorSubsystem notesensor) {
-        this.shooter2 = shooter2;
-        this.index = index;
-        this.notesensor = notesensor;
-        addRequirements(shooter2, index, notesensor); 
+        addCommands(
+            new ShootAmp(shooter2, index, notesensor).withTimeout(1));
+            // FIXME the index doesn't seem to stop spinning
     }
 
-    @Override
-    public void initialize() {
-        shooter2.setFlywheelToIdle();
-    }
-
-    @Override
-    public void execute() {
-        /* 
-        * Turn on the flywheel `setTargetFlywheelVelocity`
-        * Check if flywheel has reached target velocity `isFlywheelNominal`
-        * If true, index the game piece forward
-        * Else, setPower of Index to zero and continue running the flywheel until `isFlywheelNominal` is true
-         */
-
-        shooter2.setFlywheelVelocityAmp(ShooterConstants.FLYWHEEL_VELOCITY_AMP);
-
-        if (shooter2.isFlywheelNominalAmp()) {
-            index.setIndexPower(IndexConstants.INDEX_POWER);
-        } else {
-            index.stopIndexing();
-            // shooter2.setFlywheelVelocity(shooter2.FLYWHEEL_VELOCITY);
-        }
-    }
-
-    @Override
-    public void end(boolean interrupted) {
-        shooter2.setFlywheelVelocity(0);
-    }
-
-    @Override
-    public boolean isFinished() {
-        return false;
-    }
-} // end of class AutoShootAmp
+} // End of class AutoShootAmp 
